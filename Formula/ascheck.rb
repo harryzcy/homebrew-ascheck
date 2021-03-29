76fc6e1f8efef8364aa39e8661ec8a73bfdd8f46
Chongyi Zheng <harry@harryzheng.com>
2021-03-29 22:00:26 +0000

    ascheck: add 0.1.0 bottle.

--- Formula/ascheck.rb
+++ Formula/ascheck.rb
@@ -6,6 +6,12 @@ class Ascheck < Formula
   license "MIT"
   head "https://github.com/harryzcy/ascheck.git"
 
+  bottle do
+    root_url "https://github.com/harryzcy/homebrew-ascheck/releases/download/ascheck-0.1.0"
+    sha256 cellar: :any_skip_relocation, catalina:     "a07e726755eeae1f20d4d260136bbfcece797434c42b719a898960159052187d"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "cf79900fe888b4ab446dcf1b7fe09ef71608b76d594eac7a55dc7d11b4899707"
+  end
+
   depends_on "go" => :build
 
   def install
